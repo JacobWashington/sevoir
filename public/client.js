4296f226d61582c7e8b9f5dc48a19666ec5eca51
jacob washington <jacobwashington.dev@gmail.com>
2021-08-17 23:37:06 -0700

    computer swap

--- public/client.js
+++ public/client.js
@@ -3,8 +3,6 @@ let name;
 let connectedUser;
 
 //connecting to our signaling server
-let conn = new WebSocket("wss://sevoir-server3.web.app:9090", "wss");
-
 conn.onopen = function () {
   console.log("Connected to the signaling server");
 };
